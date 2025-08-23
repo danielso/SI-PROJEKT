03439a6b777c8fd05d1276ee456b8f5d1b28837a
danielso <soons.ni02@gmail.com>
2025-08-23 16:25:17 +0100

    zmiany

--- app/src/Form/AdminProfileFullType.php
+++ app/src/Form/AdminProfileFullType.php
@@ -2,20 +2,29 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use App\Entity\User;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 /**
  * FormType for managing the admin profile form, including email and password fields.
  */
 class AdminProfileFullType extends AbstractType
 {
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * Builds the form for editing the admin profile.
      *
@@ -24,12 +33,23 @@ class AdminProfileFullType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        /** @var User $user */
+        $user = $options['data'];
+
         $builder
             ->add('email', EmailType::class, [
                 'label' => 'Adres email',
                 'constraints' => [
                     new Assert\NotBlank(),
                     new Assert\Email(),
+                    new Assert\Callback(function ($email, ExecutionContextInterface $context) use ($user) {
+                        $existingUser = $this->em->getRepository(User::class)->findOneBy(['email' => $email]);
+                        if ($existingUser && $existingUser->getId() !== $user->getId()) {
+                            $context->buildViolation('Ten email jest już zajęty!')
+                                ->atPath('email')
+                                ->addViolation();
+                        }
+                    }),
                 ],
             ])
             ->add('newPassword', RepeatedType::class, [
